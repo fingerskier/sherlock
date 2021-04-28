ee77f3d94ea3fbf27c565a91dabaf6bcc8ac1500
fingerskier <fingerskier@gmail.com>
2021-04-28 10:49:47 -0500

    initial training

--- sherlock.js
+++ sherlock.js
@@ -1,4 +1,4 @@
-const brain = require('./brain-browser.min.js');
+const brain = require('brain.js');
 const fs = require('fs')
 const net_file = './book.json'
 
@@ -9,44 +9,6 @@ let training_data = [
   'It was love at first sight, and Spot had a frontrow seat.',
   'It was a very special moment for all.'
 ];
-// let training_data = [
-// 'To Sherlock Holmes she is always _the_ woman.',
-// 'I have seldom heard him',
-// 'mention her under any other name.',
-// 'In his eyes she eclipses and',
-// 'predominates the whole of her sex.',
-// 'It was not that he felt any emotion',
-// 'akin to love for Irene Adler.',
-// 'All emotions, ',
-// 'and that one particularly,',
-// 'were abhorrent to his cold, ',
-// 'precise but admirably balanced mind.',
-// 'He was, ',
-// 'I take it, ',
-// 'the most perfect reasoning and observing machine that',
-// 'the world has seen, ',
-// 'but as a lover he would have placed himself in a',
-// 'false position.',
-// 'He never spoke of the softer passions, ',
-// 'save with a gibe',
-// 'and a sneer.',
-// 'They were admirable things for the observer—excellent for',
-// 'drawing the veil from men’s motives and actions.',
-// 'But for the trained',
-// 'reasoner to admit such intrusions into his own delicate and finely',
-// 'adjusted temperament was to introduce a distracting factor which might',
-// 'throw a doubt upon all his mental results.',
-// 'Grit in a sensitive',
-// 'instrument, ',
-// 'or a crack in one of his own high-power lenses, ',
-// 'would not',
-// 'be more disturbing than a strong emotion in a nature such as his.',
-// 'And',
-// 'yet there was but one woman to him, ',
-// 'and that woman was the late Irene',
-// 'Adler, ',
-// 'of dubious and questionable memory.',
-// ]
 
 const lstm = new brain.recurrent.LSTM();
 
---
+++ train.js
@@ -0,0 +1,13 @@
+const brain = require('brain.js')
+const path = require('path')
+const trainer = require('../train_lstm_brain')
+
+let netFile = process.argv[2] || './model.sherlock.json'
+let dataFile = process.argv[3] || './Sherlock.txt'
+
+netFile = path.resolve(netFile)
+dataFile = path.resolve(dataFile)
+
+for (let I=3718; I < 11795; I++) {
+  trainer(netFile, dataFile, I)
+}
